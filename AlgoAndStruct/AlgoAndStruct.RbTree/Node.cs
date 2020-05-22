a16a91f8bacc37e3b36fdf9bd813b6e08bb32fd5
andrey.zvyagintsev <andrey.zvyagintsev.1993@gmail.com>
2020-05-23 02:26:31 +0500

    Solution, Project+Unit Tests Project for Red-Back Tree.

---
+++ AlgoAndStruct/AlgoAndStruct.RbTree/Node.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgoAndStruct.RbTree
+{
+    public class Node
+    {
+        public Node LeftNode { get; set; }
+
+        public Node RightNode { get; set; }
+
+        public int Value { get; set; }
+    }
+}
